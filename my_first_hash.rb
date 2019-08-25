e5203f380754105dd169d6993acb0c99d8feb3f1
Roberto Medina <robby.medina562@gmail.com>
2019-08-25 03:08:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ your_hash = {"a" =>" b"}
 
 
 
-def shipping_manifest(the_manifest)
+def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" =>3}
 
 end
